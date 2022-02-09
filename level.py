ca35f0fd29ddae8fa73478e756e14923a9429b64
dongoc <dongoc21hj@gmail.com>
2022-02-09 11:48:13 +0900

    rendering tile at level

--- level.py
+++ level.py
@@ -1,13 +1,30 @@
+from re import X
 import pygame
-
+from settings import TILESIZE, WORLD_MAP
+from tile import Tile
+from player import Player
 
 class Level:
     def __init__(self):
+
+        # get the display surface
+        self.display_surface = pygame.display.get_surface()
 
         # sprite group setup
         self.visible_sprites = pygame.sprite.Group()
         self.obstacles_sprites = pygame.sprite.Group()
 
-        def run(self):
-            # update and draw the game
-            pass
+        # sprite setup
+        self.create_map()
+
+    def create_map(self):
+        for row_index, row in enumerate(WORLD_MAP):
+            for col_index, col in enumerate(row):
+                x = col_index * TILESIZE
+                y = row_index * TILESIZE
+                if col == 'x':
+                  Tile((x, y), [self.visible_sprites])
+
+    def run(self):
+        # update and draw the game
+        pass
